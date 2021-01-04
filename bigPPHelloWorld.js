7bf313a386072b5fad0b91694f7076a98f236d8b
vedvardhan <70092988+vedvardhan@users.noreply.github.com>
2021-01-04 19:43:37 +0530

    I have a big PP

---
+++ bigPPHelloWorld.js
@@ -0,0 +1,10 @@
+var iHaveABigPP = true;
+
+var uHaveASmallPP = true;
+
+var uAreANoobCoder = true;
+
+if(1 + 1 == 2 && iHaveABigPP && uHaveASmallPP && uAreANoobCoder == true && "Sam" !=== "Chintu from Whitehat JR") {
+  console.log("Hello World")
+} else console.log("What in the world is happening rn?!!")
+
